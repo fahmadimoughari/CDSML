dfbec625c87fbf71f5f4f86e9c51419516ca4715
Fatemeh Ahmadi Moughari <49623013+fahmadimoughari@users.noreply.github.com>
2020-10-07 05:28:41 +0330

    Update manifold.py

--- Codes/CDSML/manifold.py
+++ Codes/CDSML/manifold.py
@@ -8,10 +8,10 @@ import numpy
 import numpy as np
 from numpy.linalg import inv
 
-def manifold_learning(Train_IC, P, Q, K, SimD, SimC, landa, miu):
+def manifold_learning(Train_B, P, Q, K, SimD, SimC, landa, miu):
     
     """ This function runs the manifold learning and trains the latent matrices
-        Train_IC is the training matrix 
+        Train_B is the training matrix 
         Q and P are the latent matrices
         K is the dimension of latent space
         SimD and SimC are the similarity matrices of drugs and cell lines
@@ -24,17 +24,17 @@ def manifold_learning(Train_IC, P, Q, K, SimD, SimC, landa, miu):
     while not converge:
         
         # update P
-         for i in range(len(Train_IC)):
-            P[i] = update_p(i,Train_IC, P, Q, K, SimC, landa, miu)
+         for i in range(len(Train_B)):
+            P[i] = update_p(i,Train_B, P, Q, K, SimC, landa, miu)
             
         # update Q
-         for j in range(len(Train_IC[0])):
-            Q[j] = update_q(j,Train_IC, P, Q, K, SimD, landa, miu)
+         for j in range(len(Train_B[0])):
+            Q[j] = update_q(j,Train_B, P, Q, K, SimD, landa, miu)
         
         # computing the difference in predicted matrix of two subsequnt iterations 
          new_pred = P.dot(Q.T)
          difference = 0
-         for i in range(len(Train_IC)):
+         for i in range(len(Train_B)):
              difference = difference + np.linalg.norm(old_pred[i] - new_pred[i])
          old_pred = new_pred
 #         print(difference)
@@ -46,11 +46,11 @@ def manifold_learning(Train_IC, P, Q, K, SimD, SimC, landa, miu):
     return new_pred, P, Q
 
 #-----------------------------------------------------
-def update_p(i, Train_IC, P, Q, K, SimC, landa, miu):
+def update_p(i, Train_B, P, Q, K, SimC, landa, miu):
     """ This function updates the ith row of P matrix
     
         i is the number of row
-        Train_IC is the training matrix 
+        Train_B is the training matrix 
         Q and P are the latent matrices
         K is the dimension of latent space
         SimC is the similarity matrices of cell lines
@@ -58,7 +58,7 @@ def update_p(i, Train_IC, P, Q, K, SimC, landa, miu):
         miu is the regularization coeeficient for latent matrices        
     """
     
-    pi = Train_IC[i,:].dot(Q)
+    pi = Train_B[i,:].dot(Q)
     pi = pi.reshape(1,K)
     
     xi2 = 0
@@ -85,11 +85,11 @@ def update_p(i, Train_IC, P, Q, K, SimC, landa, miu):
     
 #B(j,:)=(Y(:,j)'*A+alpha*(W_c(j,:)+(W_c(:,j))')*B)/(A'*A+alpha*sum(W_c(j,:)+(W_c(:,j))')*eye(f)+lamda*eye(f));
 #Update yj---------------------------------------
-def update_q(j, Train_IC, P, Q, K, SimD, landa, miu):
+def update_q(j, Train_B, P, Q, K, SimD, landa, miu):
     """ This function updates the jth row of Q matrix
     
         i is the number of row
-        Train_IC is the training matrix 
+        Train_B is the training matrix 
         Q and P are the latent matrices
         K is the dimension of latent space
         SimD is the similarity matrices of drugs
@@ -98,7 +98,7 @@ def update_q(j, Train_IC, P, Q, K, SimD, landa, miu):
     """
     
     
-    qj = (Train_IC[:, j].T).dot(P)
+    qj = (Train_B[:, j].T).dot(P)
     qj = qj.reshape(1, K)
     
     xi2 = 0
@@ -121,10 +121,10 @@ def update_q(j, Train_IC, P, Q, K, SimD, landa, miu):
     return final
 
     
-def loss(IC_real, P, Q, SimC, SimD, landa, miu):
+def loss(B_real, P, Q, SimC, SimD, landa, miu):
     """ This function computes the loss function in manifold learning
     
-        IC-real is the real matrix of IC values
+        B_real is the real matrix of labels
         P and Q are the latent matrices
         SimD and SimC are the similarity matrices of drugs and cell lines
         landa controls the similarity conservation while manifold learning
@@ -132,16 +132,16 @@ def loss(IC_real, P, Q, SimC, SimD, landa, miu):
     """
     
     
-    row = IC_real.shape[0]
-    col = IC_real.shape[1]
+    row = B_real.shape[0]
+    col = B_real.shape[1]
 
     diff = 0
-    IC_pred = P.dot(Q.T)
+    B_pred = P.dot(Q.T)
     
-    # the squraed error between real and predicted IC50
+    # the squraed error between real and predicted labels
     for i in range(row):
         for j in range(col):
-            diff = diff + (IC_real[i][j] - IC_pred[i][j])**2
+            diff = diff + (B_real[i][j] - B_pred[i][j])**2
     diff=diff / 2
     
     # computing the norm of P
